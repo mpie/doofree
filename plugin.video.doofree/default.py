af84a50717f92005edcc0d6eeb42fc1a85d8f7af
Mpie <info@mpie.nl>
2014-03-23 01:45:12 +0100

    Added several uk hosts and location to the mp4 files

--- plugin.video.doofree/default.py
+++ plugin.video.doofree/default.py
@@ -1,6 +1,6 @@
 import re, urllib2, urllib
 import json
-import xbmcaddon, xbmcplugin, xbmcgui
+import xbmcaddon, xbmcplugin, xbmcgui, xbmc
 
 ADDON = xbmcaddon.Addon(id='plugin.video.doofree')
 PATH = 'doofree'
@@ -11,6 +11,10 @@ addon_handle = ''
 
 master_json = "https://raw.githubusercontent.com/mpie/doofree/master/master.json"
 seesantv = "http://www.seesantv.com/seesantv_2014/"
+asia=["http://as11.seesantv.com/"]
+uk=["http://uk23.seesantv.com/", "http://uk24.seesantv.com/", "http://uk25.seesantv.com/", "http://uk1.seesantv.com/", "http://uk27.seesantv.com/"]
+us=["http://us14.seesantv.com/"]
+
 base_url = sys.argv[0]
 addon_handle = int(sys.argv[1])
 
@@ -42,13 +46,21 @@ def addThaiDir(name, url, mode, image):
     ok = addDirItem(url, name, image)
     return ok
 
-def addThaiLink(name, url, mode, image):
-    url = build_url({'mode': mode, 'name': name.encode('tis-620'), 'url': url})
+def addThaiLink(name, url, mode, image, channel):
+    url = build_url({'mode': mode, 'name': name.encode('tis-620'), 'url': url, 'channel': channel})
     item = xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=image)
     contextMenuItems = []
     item.addContextMenuItems(contextMenuItems, replaceItems=True)
     ok = xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=item, isFolder=False)
     return ok
+
+def exists(url):
+    try:
+        r = urllib2.urlopen(url, timeout=1)
+        if r.getcode() == 200:
+            return True
+    except urllib2.URLError, e:
+        return False
 
 def HOME():
     data = parseJson(master_json)
@@ -72,29 +84,51 @@ def getEpisodes(url):
     link = getContent(url + '&vdo_type=.mp4')
     link = ''.join(link.splitlines()).replace('\'','"')
     link=''.join(link.splitlines()).replace('<i class="icon-new"></i>','')
+
     episodematch = re.compile('<table class="program-archive">(.+?)</table>').findall(link)
-    #print episodematch
     episodes = re.compile('<a href="(.+?)" >(.+?)</a> </td>                           \t\t\t\t\t\t\t<td>\t\t\t\t\t\t\t\t<a href="(.+?)" ><img').findall(episodematch[0])
-    #print episodes
+
     programMeta = re.compile('<div class="program-meta">(.+?)</div>').findall(link)
     image = re.compile('<img src="(.+?)" alt').findall(programMeta[0])[0]
+
+    topInfo = re.compile('<div class="top-info">(.+?)</div>').findall(link)
+    channel = re.compile('<img src="(.+?)" width').findall(topInfo[0])
+    if (len(channel) > 0):
+        channel = 'ch' + channel[0][-5]
+        if (channel == 'chv'):
+            channel = 'chthaipbs'
+    else:
+        channel = 'chall'
+    
     for episode in episodes:
-        #print episode[1].decode('tis-620')
-        addThaiLink(episode[1].decode('tis-620'), seesantv + episode[0], 3, image)
+        addThaiLink(episode[1].decode('tis-620'), seesantv + episode[0], 3, image, channel)
     # paginator
     paginator=re.compile('<div class="page_list"  align="center">(.+?)</ul>').findall(link)[0]
-    #print paginator
     pages=re.compile('>(\d+)</a>').findall(paginator)
-    if len(pages)>1:
+    if (len(pages) > 1):
         for page in pages:
             pag = int (page[0]) - 1
             pageUrl = url + '&vdo_type=.mp4&page=' + str (pag)
             addThaiDir('page ' + page[0], pageUrl, 2, image)
-                
+
+def getVideoUrl(name, url, channel):
+    programId = re.compile('program_id=(\d+)').findall(url)[0]
+    if (len(programId) < 5):
+        programId = '0' + programId
+    fullDate = re.compile('(\d+-\d+-\d+) ').findall(name)[0]
+    date = ''.join(fullDate.splitlines()).replace('-','')
+    path = channel + '/' + programId + '/' + date + '1-' + programId + '.mp4'
+    for host in uk:
+        fullurl = host + path
+        found = exists(fullurl)
+        if (found):
+            xbmc.Player().play(fullurl)
+            break
+    
 def getParams():
     param = []
     paramstring = sys.argv[2]
-    if len(paramstring)>= 2:
+    if (len(paramstring)>= 2):
         params = sys.argv[2]
         cleanedparams = params.replace('?','')
         if (params[len(params)-1] == '/'):
@@ -104,7 +138,7 @@ def getParams():
         for i in range(len(pairsofparams)):
             splitparams = {}
             splitparams = pairsofparams[i].split('=')
-            if (len(splitparams)) == 2:
+            if (len(splitparams) == 2):
                 param[splitparams[0]] = splitparams[1]
     return param
 
@@ -113,7 +147,7 @@ url=None
 name=None
 mode=None
 serverurl=None
-pageurl=None
+channel=None
 playpath=None
 try:
     url=urllib.unquote_plus(params['url'])
@@ -132,7 +166,7 @@ try:
 except:
     pass
 try:
-    pageurl=urllib.unquote_plus(params['pageurl'])
+    channel=urllib.unquote_plus(params['channel'])
 except:
     pass
 try:
@@ -142,33 +176,12 @@ except:
 
 sysarg=str(sys.argv[1])
 if mode==None or url==None or len(url)<1:
-    #getEpisodes("http://www.seesantv.com/seesantv_2014/program_detail.php?id=5409")
     HOME()
 elif mode==1:
     INDEX(name, url)
 elif mode==2:
     getEpisodes(url)
-elif mode==4:
-    SEARCH(url)
-elif mode==6:
-    ChooseServerReg(url)
-elif mode==7:
-    getVideoUrl(url)
-elif mode==8:
-    GetLoginCookie(cj, cookiefile)
-elif mode==9:
-    GetServerList("/sorted_01_0111.txt",url)
-elif mode==10:
-    GetServerList("/sorted_01_0110.txt",url)
-elif mode==11:
-    GetServerList("/sorted_02_0112.txt",url)
-elif mode==12:
-    GetServerList("/sorted_01_0711.txt",url)
-elif mode==13:
-    showLiveTV()
-elif mode==14:
-    playVideo(serverurl, url)
-elif mode==15:
-    playTv(serverurl)
+elif mode==3:
+    getVideoUrl(name, url, channel)
 
 xbmcplugin.endOfDirectory(addon_handle)
